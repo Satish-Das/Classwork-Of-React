e7d6b6c53f6c43159479dd239a4767d8eef7130d
Satish-Das <dasskumar9801@gmail.com>
2025-01-30 13:59:23 +0530

    replace ApiTesting component with UserContextFinal in main entry point

--- project-1/src/main.jsx
+++ project-1/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import ApiTesting from "./lecture16_01/useeffectexer";
+// import ApiTesting from "./lecture16_01/useeffectexer";
+import UserContextFinal from "./lecture30_01_25/useContext3";
 // import Wrapper from "./lecture15_01/Wrapper";
 // import MyComponent from './comp1';
 // import StateExample from './lec26_12/stateExam1'
@@ -10,6 +11,6 @@ import ApiTesting from "./lecture16_01/useeffectexer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ApiTesting />
+    <UserContextFinal />
   </StrictMode>
 );
